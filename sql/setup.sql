6ba61546b413e4fa65c7dd5b6f9ec8fa454fe204
Patrick Wilson <pmwilson333@gmail.com>
2020-09-22 16:53:48 -0700

    log table built and original tests passing

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,22 @@
+DROP TABLE IF EXISTS logs;
 DROP TABLE IF EXISTS recipes;
 
 CREATE TABLE recipes (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
   name TEXT NOT NULL,
   directions TEXT[],
-  ingredients JSONB
+  ingredients JSONB,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE logs (
+  log_id BIGINT GENERATED ALWAYS AS IDENTITY,
+  id BIGINT,
+  date_of_event TEXT NOT NULL,
+  notes TEXT,
+  rating INT,
+  PRIMARY KEY(log_id),
+  CONSTRAINT fk_recipe
+    FOREIGN KEY (id) 
+      REFERENCES recipes(id)
 );
